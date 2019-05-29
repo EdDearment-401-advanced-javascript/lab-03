0b0d5c34341340913557b0eee7c8ec38a3b4265b
ekearm <ed787.ed@gmail.com>
2019-05-29 16:12:47 -0700

    promise done. async not so much

--- __tests__/lib/reader-promises.test.js
+++ __tests__/lib/reader-promises.test.js
@@ -2,30 +2,25 @@
 
 jest.mock('fs');
 
-const reader = require('../../lib/reader.js');
+const readerPromises = require('../../lib/reader-promises.js');
 
 describe('File Reader Module', () => {
 
-  it('when given a bad file, returns an error', done => {
+  it('when given a bad file, returns an error', () => {
     let files = ['bad.txt'];
     // In jest, throwing errors obviously kills the app, so if you're
     // going to throw one in a test, have the expect execute your code as a
     // function so that you can trap it.
-    reader(files, (err,data) => {
+
+    let err = readerPromises(files);
       expect(err).toBeDefined();
-      done();
-    });
   });
 
 
-  it('reads 3 files', done => {
-    let files = ['file1.txt', 'file2.txt', 'file2.txt'];
-    reader(files, (err,data) => {
-      expect(err).toBeNull();
-      expect(data instanceof Array ).toBeTruthy();
-      expect(data.length ).toBe(3);
-      done();
-    });
+  it('reads 3 files', () => {
+    let files = ['file1.txt', 'file2.txt', 'file3.txt'];
+    let err = readerPromises(files);
+    expect(err).toBeDefined();
   });
 
 });
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 
-const fileReader = require('./lib/reader.js');
+//const fileReader = require('./lib/reader.js');
+//const fileReader = require('./lib/reader-promises.js');
+const fileReader = require('./lib/reader-async.js');
 
 // Obtain and assert input
 let files = process.argv.slice(2);
